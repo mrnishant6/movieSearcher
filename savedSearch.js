c1a5cc51d704c667ad5a043e730e3c15fce444b0
NISHANT KUMAR <mr.nishant.6@gmail.com>
2018-09-07 08:46:13 +0530

    Create savedSearch.js

---
+++ savedSearch.js
@@ -0,0 +1,17 @@
+import  React from  'react'
+
+class Savedsearch extends React.Component{
+  constructor(props){
+      super(props)
+      console.log(props);
+  }
+  
+  
+    render() {
+        return(
+            <div></div>
+      );
+    }
+  }
+
+export default Savedsearch;
